7b16552c560fdbec59733857138f74d74def4c02
Abhishek gupta <abhishekgupta241120@gmail.com>
2026-09-23 21:07:24 +0530

    ✅ ECHOECHO | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/ECHOECHO/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    string s1 ,s2, s3,s4;
+    cin>>s1>>s2>>s3>>s4;
+    if(s1 == s3 and s2 ==s4){
+        cout<<"yes"<<endl;
+        
+    }else{
+        cout<<"no"<<endl;
+    }
+}
